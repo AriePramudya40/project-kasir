8895ca7273f34faa7700285d08a4e77b05d25437
Arie Pramudya <134258924+Ariepramudya01@users.noreply.github.com>
2026-02-08 13:14:00 +0700

    menghapus cdn

--- resources/views/pos/dashboard.blade.php
+++ resources/views/pos/dashboard.blade.php
@@ -283,8 +283,6 @@
         data-error="{{ session('error') }}">
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
     <script>
         // --- JAM DIGITAL ---
         function updateClock() {
